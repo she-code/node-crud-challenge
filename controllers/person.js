f993ed3a80ec1ee92b5cd923be545ae6b3791146
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2024-10-28 22:28:20 +0300

    Separated into CRUD structure

--- controllers/person.js
+++ controllers/person.js
@@ -1,11 +1,9 @@
-const app = require("../index");
-// let Person = app.get("db");
+const AppError = require("../utils/appError");
+let persons = global.db;
 
 exports.getPersons = async (req, res, next) => {
   try {
     const { personId } = req.params;
-    let Person = global.db;
-    console.log({ Person });
     if (personId) {
       const person = persons.find((p) => p.id === personId);
       if (person) {
@@ -14,10 +12,6 @@ exports.getPersons = async (req, res, next) => {
           data: person,
         });
       } else {
-        // return res.status(404).json({
-        //   status: "fail",
-        //   message: "Person not found",
-        // });
         return next(new AppError("Person not found", 404));
       }
     }
@@ -29,6 +23,89 @@ exports.getPersons = async (req, res, next) => {
   } catch (error) {
     console.error(error);
 
+    return next(error);
+  }
+};
+// POST /person - Create a new person
+exports.addPerson = async (req, res, next) => {
+  try {
+    const { name, age, hobbies } = req.body;
+
+    if (!name || typeof age !== "number" || !Array.isArray(hobbies)) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Invalid input data",
+      });
+    }
+
+    const newPerson = {
+      id: uuidv4(),
+      name,
+      age,
+      hobbies,
+    };
+
+    persons.push(newPerson);
+    res.status(201).json({
+      status: "success",
+      data: newPerson,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// PUT /person/:personId - Update an existing person
+exports.updatePerson = async (req, res, next) => {
+  try {
+    const { personId } = req.params;
+    const { name, age, hobbies } = req.body;
+
+    const personIndex = persons.findIndex((p) => p.id === personId);
+    if (personIndex === -1) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Person not found",
+      });
+    }
+
+    if (!name || typeof age !== "number" || !Array.isArray(hobbies)) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Invalid input data",
+      });
+    }
+
+    persons[personIndex] = { ...persons[personIndex], name, age, hobbies };
+    res.status(200).json({
+      status: "success",
+      data: persons[personIndex],
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// DELETE /person/:personId - Delete a person
+exports.deletePerson = async (req, res, next) => {
+  try {
+    const { personId } = req.params;
+    const personIndex = persons.findIndex((p) => p.id === personId);
+
+    if (personIndex === -1) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Person not found",
+      });
+    }
+
+    const deletedPerson = persons.splice(personIndex, 1)[0];
+    res.status(200).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    console.error(error);
     return next(error);
   }
 };
--- routes/person.js
+++ routes/person.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const route = express.Router();
 const personController = require("../controllers/person");
-route.get("/person", personController.getPersons);
+route.get("/:personId?", personController.getPersons);
+route.put("/:personId", personController.updatePerson);
+route.delete("/:personId", personController.deletePerson);
+route.post("/", personController.addPerson);
 
 module.exports = route;
